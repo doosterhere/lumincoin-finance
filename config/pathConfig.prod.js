ac0d8fa6ffae5ae5b936e06623d1e745e823aa9a
Mikalai Shcharbakou <doosterhere@gmail.com>
2023-05-15 12:05:58 +0300

    another build has been made

--- config/pathConfig.prod.js
+++ config/pathConfig.prod.js
@@ -1,3 +1,4 @@
 export default {
-    host: 'https://lumincoin-backend.onrender.com/api'
+    // host: 'https://lumincoin-backend.onrender.com/api'
+    host: 'https://iron-bald-sundae.glitch.me/api'
 }
